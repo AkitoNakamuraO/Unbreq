6846b65b3c04e03890a34f22819a1265db2a675e
Akito Nakamura <akito.nakamura.c@gmail.com>
2020-12-30 11:48:11 +0900

    no message

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ async function handleEvent(event) {
     if (message == '登録') {
         const area1 = ['北海道', '東北', "関東", "中部", "関西", "中国", "四国", "九州・沖縄"]; //１番大きな地域のくくりを格納する
         responseMessage = createMessage(area1); //area1中身ボタンメッセージで返信内容に入れる
-    } else if (message != '登録' && message != '天気教えて' && message != '06~12時の時間帯' && message != '12~18時の時間帯' && message != '18~24時の時間帯') { //入力されたテキストが地地域の名前の時処理する
+    } else if (message != '登録') { //入力されたテキストが地地域の名前の時処理する
         //SQL文
         let sql = 'select * from city where area1 = ?;'; //cityテーブルからmessageと同じ場所をdataに格納
         let data = await connectionSql(sql, message); //データを取得
@@ -266,6 +266,51 @@ async function handleEvent(event) {
             responseMessage = createMessage(area3); //area3中身ボタンメッセージで返信内容に入れる
 
             if (area3.length == 0) {
+                //semは場合分けに使う
+                let sem = 0;
+                let judge;
+
+                //「天気教えて」で反応する。
+                if (message == '天気教えて') {
+                    //知りたい時間帯の選択肢を表示する。
+                    let timeMessage = require('./GettingTheWeather/time.json');
+                    await client.replyMessage(event.replyToken, timeMessage);
+                    sem = 1;
+                } else if (sem == 1) { //上の天気教えてを通ってからでないとここには入れない
+                    sem = 0;
+                    //getWather(event,codeId,time)で傘が必要か判断する
+                    //codeIdは各地域のコード(time.jsonを参照)
+                    //timeは選択した時間帯のテキスト
+                    //getWeatherの返り値(judge)で場合分け
+                    judge = await getWeather(event, "070030", message);
+                    console.log(judge);
+                    //1は0~30%,2は30~50%,3は50~100%
+                    if (judge == 1) {
+                        responseMessage = {
+                            type: 'text',
+                            text: '今日傘いらないよ！'
+                        };
+                    } else if (judge == 2) {
+                        responseMessage = {
+                            type: 'text',
+                            text: '今日折り畳み傘持ってくといいかも！'
+                        };
+                    } else if (judge == 3) {
+                        responseMessage = {
+                            type: 'text',
+                            text: '今日傘いるよ！'
+                        };
+                    }
+                }
+
+                // else { //天気教えて以外はここに入る
+                //     responseMessage = {
+                //         type: 'text',
+                //         text: '天気教えてって言ってね！'
+                //     };
+                // }
+
+                //すで登録しているユーザーかどうかを判定するためにデータベースの中身を調べる
                 const userData = await getUserDB(event.source.userId);
                 //選択した地域のシティコードをユーザーIDとセットでuserデータベースに保存する
                 await insertData(userData, event.source.userId, await getCityCode(message));
@@ -276,50 +321,6 @@ async function handleEvent(event) {
                 };
             }
         }
-    }
-
-
-
-    //semは場合分けに使う
-    let sem = 0;
-    let judge;
-
-    //「天気教えて」で反応する。
-    if (message == '天気教えて') {
-        //知りたい時間帯の選択肢を表示する。
-        let timeMessage = require('./GettingTheWeather/time.json');
-        await client.replyMessage(event.replyToken, timeMessage);
-        sem = 1;
-    } else if (sem == 1) { //上の天気教えてを通ってからでないとここには入れない
-        sem = 0;
-        //getWather(event,codeId,time)で傘が必要か判断する
-        //codeIdは各地域のコード(time.jsonを参照)
-        //timeは選択した時間帯のテキスト
-        //getWeatherの返り値(judge)で場合分け
-        judge = await getWeather(event, "070030", message);
-        console.log(judge);
-        //1は0~30%,2は30~50%,3は50~100%
-        if (judge == 1) {
-            responseMessage = {
-                type: 'text',
-                text: '今日傘いらないよ！'
-            };
-        } else if (judge == 2) {
-            responseMessage = {
-                type: 'text',
-                text: '今日折り畳み傘持ってくといいかも！'
-            };
-        } else if (judge == 3) {
-            responseMessage = {
-                type: 'text',
-                text: '今日傘いるよ！'
-            };
-        }
-    } else { //天気教えて以外はここに入る
-        responseMessage = {
-            type: 'text',
-            text: '天気教えてって言ってね！'
-        };
     }
 
     return client.replyMessage(event.replyToken, responseMessage);
